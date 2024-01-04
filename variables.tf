1c202ae3e64662df71965b136708c85490118e83
bkylee <brian.ky.lee@outlook.com>
2024-01-03 21:47:46 -0500

    created terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable TENANT_ID { 8f06ae15-e337-408c-abe2-da17244ad3a }
+
+# data for dev subscription and service principal 
+variable SUBSCRIPTION_ID_DEV { d8d24b72-a95a-4d2b-a290-38110a3a1859 }
+variable SERVICE_PRINCIPLE_ID_DEV { d2ba1ebe-e742-40eb-85b5-26e6cc2f3c91 }
+variable SERVICE_PRINCIPLE_SECRET_DEV { Net8Q~I1c5mnrWaO6xeegBNoe7bKLkZ0aZpbHbkV }
+
+# data for prod subscription and service principal 
+variable SUBSCRIPTION_ID_PROD { e8e5433f-c2fe-48bd-b6b4-a37aaf401663 }
+variable SERVICE_PRINCIPLE_ID_PROD { 405a6362-f1e5-4d6b-8b65-8e9790868f34 }
+variable SERVICE_PRINCIPLE_SECRET_PROD { syW8Q~y5jOOLzmSvfyNohjSRKbGrSiYsCgXhwdc }
