106018a77f300391b52e7140a1fd0bafd68e6c39
bkylee <brian.ky.lee@outlook.com>
2024-01-07 18:39:04 -0500

    made basic header and resume page

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="subsection">Home</div>
+      <div className="subsection">Blog</div>
+      <div className="subsection">Resume</div>
+      <div className="subsection">Projects</div>
+      <div className="subsection">About</div>
+      <div className="subsction">Videos</div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/ResumePage.jsx
@@ -0,0 +1,13 @@
+import Header from 'Header'
+import Resume from './Resume'
+
+const ResumePage = () => {
+  return (
+    <>
+      <Header />
+      <Resume />
+    </>
+  )
+}
+
+export default ResumePage
