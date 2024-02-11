48970b9e55f1f4422daa6ce62319d1e304388244
bkylee <brian.ky.lee@outlook.com>
2024-02-11 11:14:12 -0500

    troubleshooting why the api call is not retrieving the json

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
     <header>
       <h1>Brian&apos;s Cool Website</h1>
       <div className="headButtons">
-        <a>Home</a>
+        <button className="subsection">Home</button>
         <button className="subsection">Blog</button>
         <button className="subsection">Resume</button>
         <button className="subsection">Projects</button>
         <button className="subsection">About</button>
         <button className="subsction">Videos</button>
-        <Visitors />
       </div>
+      <Visitors />
     </header>
   )
 }
--- src/Visitors.jsx
+++ src/Visitors.jsx
@@ -1,19 +1,26 @@
 import { useEffect } from "react";
 import { useState } from "react"
 
-function Visitors() {
-  const [data, setData] = useState(null);
+const Visitors = () =>{
+  const [count, setCount] = useState(null);
 
   useEffect(() => {
-    fetch('https://g895njd.azurewebsites.net/api/visitorcount')
+    fetch('https://g895njd.azurewebsites.net/api/visitorCount?', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
       .then(response => response.json())
-      .then(data => setData(data))
+      .then(data => setCount(data.count))
       .catch(error => console.error('Error:', error));
   }, []);
 
   return (
-    <div>{data && <div>{ JSON.stringify(data)} </div>}</div>
-  )
-}
+    <div>
+      {count !== null ? `Visitor count: ${count}` : 'Loading...'}
+    </div>
+  );
+};
 
 export default Visitors
