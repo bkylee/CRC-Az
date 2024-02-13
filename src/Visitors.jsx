a7ce74966201340ea563ee54e9d11aa5391a287a
bkylee <brian.ky.lee@outlook.com>
2024-02-12 23:00:50 -0500

    I didn't really do anything

--- src/Visitors.jsx
+++ src/Visitors.jsx
@@ -5,16 +5,16 @@ const Visitors = () =>{
   const [count, setCount] = useState(null);
 
   useEffect(() => {
-    fetch('https://g895njd.azurewebsites.net/api/visitorCount?', {
+    fetch('https://g895njd.azurewebsites.net/api/visitorcount', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     })
       .then(response => response.json())
-      .then(data => setCount(data.count))
+      .then(data => { setCount(data.count); })
       .catch(error => console.error('Error:', error));
-  }, []);
+  }, []); // empty dependency array = run once at mount 
 
   return (
     <div>
