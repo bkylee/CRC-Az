dd5f987888ec18ccd539a5e40a2df6806300ab90
bkylee <brian.ky.lee@outlook.com>
2024-03-05 22:51:17 -0500

    testing to see if index is getting uploaded to web container

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <>
             <Header />
             <p>Hi! I&apos;m a Korean-born Canadian with experience in IT administration looking to specialize and pursue a career in Cloud Computing. The goal with this website and future projects is to build up my technical skills for potential Cloud Architect/Engineering roles. Also, I enjoy learning and tinkering with technology, so this has become one of my new hobbies.</p>
-            <p>Feel free to reach our via my LinkedIn</p>
+            <p>Feel free to reach our via my LinkedIn above</p>
     </>
   )
 }
